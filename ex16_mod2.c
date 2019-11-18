b333685ae88eb3b48acf6ecc3e9fa629924671eb
Jules  Octave <jbi.octave@gmail.com>
2019-11-18 09:51:46 +0000

    Add files via upload
    first way

---
+++ ex16_mod2.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+typedef struct {
+	char *name;
+	int age;
+	int height;
+	int weight;
+}Person;
+
+
+void Person_print(Person who)
+{
+	printf("Name: %s\n", who.name);
+	printf("\tAge: %d\n", who.age);
+	printf("\tHeight: %d\n", who.height);
+	printf("\tWeight: %d\n", who.weight);
+}
+int main(int argc, char *argv[]){
+	
+	Person joe; 
+	joe.name="Joe Alex";
+	joe.age=32;
+	joe.height=64;
+	joe.weight=140;
+
+	Person frank ={.name="Frank Blank",.age=20,.height=72, .weight=180};
+
+	printf("Joe is at memory location %p:\n", joe);
+	Person_print(joe);
+	printf("Frank is at memory location %p\n", frank);
+	Person_print(frank);
+
+	joe.age += 20;
+	joe.height -= 2;
+	joe.weight += 40;
+	Person_print(joe);
+
+	frank.age += 20;
+	frank.weight += 20;
+	Person_print(frank);
+
+	return 0;
+}
+
+
