0d7817d1cae15c4603e0142309a45674765299a8
Jules  Octave <jbi.octave@gmail.com>
2020-06-16 19:26:36 +0100

    Update assignment_week3.c

--- assignment_week3.c
+++ assignment_week3.c
@@ -3,15 +3,45 @@
 #include <time.h>
 #include <ctype.h>
 
+/* declaration */
 typedef struct list { int data; struct list* next; } list;
+/* declare functions to be used */
+int is_empty(const list *l);
+list * create_list(int d);
+list * add_to_front(int d, list* h);
+list* array_to_list(int d[], int size);
+void print_list( list *h , char *title);
+void swap(list *a, list *b);
+void sort_list(list *h);
 
+int main() {
+
+list* head = NULL;
+
+int i;
+int arr_r[100];
+
+srand(time(NULL));
+for(i = 0; i < 100; i++)
+	arr_r[i] = rand()%200;
+
+head = array_to_list(arr_r, 100);
+
+print_list(head, "arr_r[100] made into 100 element list");
+printf("\n\n");
+sort_list(head);
+print_list(head, "Sorted listof 100 elements");
+
+return EXIT_SUCCESS;
+}
+/* define the declared  functions */
 int is_empty(const list *l) { return (l==NULL); }
 
 list * create_list(int d)
-{
+{	// allocate memory for list
 	list* head  = malloc(sizeof(list));
-	head->data = d;
-	head->next = NULL;
+	head->data = d; // add data element to list
+	head->next = NULL; // next linked list not yet created
 	return head;
 }
 
@@ -35,75 +65,47 @@ list* array_to_list(int d[], int size)
 
 void print_list( list *h , char *title)
 {
-	int count =0;
+	int count = 0;
 	printf("%s\n", title);
 	while(h != NULL) {
-		++count;
-		if(!(count%21)) 
+		// ensure list display in 5 rows
+		if(!(count%21))
 			printf("\n");
-		printf("%d: ", h->data);
+		printf("%4d", h->data);
+                if(h->next) {
+			printf(":\t");
+		} else { printf("\n");}
 		h= h->next;
+		count++;
 	}
 }
 
-
-void swap(int *a, int *b)
+void swap(list *a, list *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+	int temp = a->data;
+	a->data = b->data;
+	b->data = temp;
 }
-/*
-void print_array(int how_many, int data[], char *str)
-{
-	int i;
-	printf("%s", str);
 
-	for (i = 0; i < how_many; i++)
-		printf("%d\t", data[i]);
-}
-*/
-void bubble (int data[], int how_many)
-{
-	int i, j;
-
-	for( i = 0; i < how_many; i++) {
-		//print_array(how_many, data, "\ninside bubble\n");
-		for(j = how_many - 1; j > i; j--)
-			if(data[j-1] > data[j])
-				swap(&data[j-1], &data[j]);
+void sort_list(list *h){
+	int s;
+	list *begin;
+	list *another = NULL;
+	if(is_empty(h))
+		return;
+	do
+	{
+		s = 0;
+		begin = h;
+		while(begin->next != another)
+		{
+			if( begin->data > begin->next->data)
+			{
+				swap(begin, begin->next);
+					s = 1;
+			}
+			begin = begin->next;
+		}
+			another = begin;
+		}while(s);
 	}
-}
-
-
-
-int main() {
-
-list* head = NULL;
-
-int i;
-int arr_r[100];
-
-srand(time(NULL));
-for(i = 0; i < 100; i++)
-	arr_r[i] = rand()%200;
-bubble(arr_r, 100);
-
-head = array_to_list(arr_r, 100);
-
-print_list(head, "arr_r[100] made into 100 element list");
-printf("\n\n");
-/*
-for(i = 0; i < 100; i++) {
-	if(!(i%11))
-		printf("\n");
-	printf("%d\t", arr_r[i]);
-}
-*/
-
-
-
-
-
-return EXIT_SUCCESS;
-}
